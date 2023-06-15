57a4a356c004865bc4529437b4764f4436ec5c52
Ethan Frey <ethanfrey@users.noreply.github.com>
2023-06-15 17:58:03 +0200

    cw20 happy path works with fixed gas prices

--- integration/src/cw20.spec.ts
+++ integration/src/cw20.spec.ts
@@ -16,6 +16,7 @@ import {
 describe("Cw20 Test Cases", () => {
   describe("happyPath", () => {
     fit("works with direct signer", async () => {
+      const signer = faucet.address0;
       const wallet = await DirectSecp256k1HdWallet.fromMnemonic(faucet.mnemonic, defaultWalletOptions);
       const tendermintClient = await Tendermint37Client.connect(pulsarium.tendermintUrl);
       const client = await SigningCosmWasmClient.createWithSigner(
@@ -25,11 +26,13 @@ describe("Cw20 Test Cases", () => {
       );
 
       // store code
-      const wasm = fs.readFileSync(__dirname + "/../../packages/app/fixtures/cw20_base.wasm");
+      const wasmBuf = fs.readFileSync(__dirname + "/../../packages/app/fixtures/cw20_base.wasm");
+      const wasm = new Uint8Array(wasmBuf); // not sure if this is needed but something is odd here (always out of gas)
       console.info(`Wasm size: ${wasm.length} bytes`);
-      // const uploadFee = calculateFee(500_000_000, defaultGasPrice);
-      // const uploadReceipt = await client.upload(faucet.address0, wasm, uploadFee, "Upload cw20_base");
-      const uploadReceipt = await client.upload(faucet.address0, wasm, 50_000_000, "Upload cw20_base");
+      const uploadFee = calculateFee(50_000_000, defaultGasPrice);
+      console.info(`Upload fee: ${JSON.stringify(uploadFee)}`);
+
+      const uploadReceipt = await client.upload(signer, wasm, uploadFee);
       console.info(`Upload succeeded. Receipt: ${JSON.stringify(uploadReceipt)}`);
       expect(uploadReceipt).toBeTruthy();
       const { codeId } = uploadReceipt;
@@ -42,34 +45,27 @@ describe("Cw20 Test Cases", () => {
         decimals: 6,
         initial_balances: [
           {
-            address: faucet.address0,
+            address: signer,
             amount: "50000000", // 50 PULSE
           },
         ],
       };
 
       const instantiateFee = calculateFee(500_000, defaultGasPrice);
-      const { contractAddress } = await client.instantiate(
-        faucet.address0,
-        codeId,
-        initMsg,
-        "PULSE Token",
-        instantiateFee,
-        {
-          memo: `Create a hackatom instance in deploy_hackatom.js`,
-          admin: faucet.address0,
-        }
-      );
+      const { contractAddress } = await client.instantiate(signer, codeId, initMsg, "PULSE Token", instantiateFee, {
+        memo: `Create a hackatom instance in deploy_hackatom.js`,
+        admin: signer,
+      });
       expect(contractAddress).toBeTruthy();
 
       // query balances
       const recipient = makeRandomAddress();
       const myBal = await client.queryContractSmart(contractAddress, { balance: { address: faucet.address0 } });
       console.info(`My balance: ${JSON.stringify(myBal)}`);
-      expect(myBal.amount).toEqual(50_000_000);
+      expect(myBal.balance).toEqual("50000000");
       const yourBal = await client.queryContractSmart(contractAddress, { balance: { address: recipient } });
       console.info(`Your balance: ${JSON.stringify(yourBal)}`);
-      expect(yourBal.amount).toEqual(0);
+      expect(yourBal.balance).toEqual("0");
     });
 
     //   describe("simulate", () => {
