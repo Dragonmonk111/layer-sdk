e7b5d030f34f3ac3bd2d27f51b7efe87945199ee
Ethan Frey <ethanfrey@users.noreply.github.com>
2023-11-19 13:05:29 +0100

    Update fixture contracts, add abstract manager

--- packages/app/fixtures/download.sh
+++ packages/app/fixtures/download.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 # Add source of any fixtures here, so we can bump versions if needed
+## Get the abstract one as well to test large sizes
 FILES=(
-https://github.com/CosmWasm/cosmwasm/releases/download/v1.2.6/hackatom.wasm
-https://github.com/CosmWasm/cw-plus/releases/download/v1.0.1/cw20_base.wasm
+https://github.com/CosmWasm/cosmwasm/releases/download/v1.4.1/hackatom.wasm
+https://github.com/CosmWasm/cw-plus/releases/download/v1.1.1/cw20_base.wasm
+https://github.com/AbstractSDK/abstract/raw/v0.19.2/framework/artifacts/abstract_manager.wasm
 )
 
 for file in ${FILES[@]}; do
   echo "Downloading $file"
   curl -L -s -O "$file"
 done
+
+# compress cw20 for testing
+
+gzip -k -f cw20_base.wasm
