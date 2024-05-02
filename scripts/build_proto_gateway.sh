deaf0420bd3faeedc9d353cb3901faf618fc6625
Ethan Frey <ethanfrey@noreply.github.com>
2024-05-02 22:47:53 +0200

    Better permissions in proto build script

--- scripts/build_proto_gateway.sh
+++ scripts/build_proto_gateway.sh
@@ -5,5 +5,7 @@ IMAGE="bufbuild/buf:1.23.1"
 
 # This generates grpc-gateway bindings in the gateway directory 
 
-docker run --rm -w /buf/proto -v $(pwd)/proto:/buf/proto -v $(pwd)/gateway:/buf/gateway "$IMAGE" ls-files
-docker run --rm -w /buf/proto -v $(pwd)/proto:/buf/proto -v $(pwd)/gateway:/buf/gateway "$IMAGE" generate
+WHOAMI=$(whoami)
+sudo docker run --rm -w /buf/proto -v $(pwd)/proto:/buf/proto -v $(pwd)/gateway:/buf/gateway "$IMAGE" ls-files
+sudo docker run --rm -w /buf/proto -v $(pwd)/proto:/buf/proto -v $(pwd)/gateway:/buf/gateway "$IMAGE" generate
+sudo chown -R $WHOAMI:$WHOAMI ./gateway
