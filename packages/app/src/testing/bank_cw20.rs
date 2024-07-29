5f21ade7778fc83c6e8408332e717d1e01591790
Ethan Frey <ethanfrey@noreply.github.com>
2024-07-29 23:48:07 +0200

    Test sending cw20 tokens to contract as best we can

--- packages/app/src/testing/bank_cw20.rs
+++ packages/app/src/testing/bank_cw20.rs
@@ -7,7 +7,9 @@ use crate::testing::utils::*;
 use crate::PulsarResult;
 
 // v1.2.6
+const HACKATOM: &[u8] = include_bytes!("../../fixtures/hackatom.wasm");
 const CW20: &[u8] = include_bytes!("../../fixtures/cw20_base.wasm");
+
 const DENOM: &str = "unative";
 const INIT_BAL_SENDER: u128 = 100_000_000;
 const INIT_BAL_GOV: u128 = 1_000_000;
@@ -280,4 +282,137 @@ fn basic_bank_messages() {
     assert_eq!(cw20.u128(), init_tokens - burn_amount);
     let cw20 = query_cw20_balance_as_native(&app, &contract, &sender);
     assert_eq!(cw20.u128(), init_tokens - transfer_amount - burn_amount);
+}
+
+#[test]
+fn contracts_send_receive_cw20_as_native() {
+    let SetupData {
+        mut app,
+        signer,
+        code_id,
+        gov_key,
+    } = setup("/tmp/slay3r/basic-bank-messages");
+    let sender = signer.account_id();
+    let gov = gov_key.account_id();
+
+    // create contract instance with 20_000_000 tokens
+    let init_tokens = 20_000_000u128;
+    let token = init_token(&mut app, code_id, "SEND", &signer, init_tokens);
+
+    // let's make a hackatom contract storing some of those tokens
+    let verify_key = PrivateKey::random();
+    let verifier = verify_key.to_pubkey().account_id().unwrap();
+
+    // create contract instance with 8_000_000 tokens
+    let escrow_tokens = 8_000_000u128;
+    let cw20_denom = format!("cw20:{}", token);
+    let escrow = init_hackatom(
+        &mut app,
+        &signer,
+        &verifier,
+        &gov,
+        escrow_tokens,
+        &cw20_denom,
+    );
+
+    // verify balances - funds were transfered as part of instantiate (MessageInfo)
+    let cw20 = query_cw20_balance_as_native(&app, &token, &sender);
+    assert_eq!(cw20.u128(), init_tokens - escrow_tokens);
+    let cw20 = query_cw20_balance_as_native(&app, &token, &gov);
+    assert_eq!(cw20.u128(), 0);
+    let cw20 = query_cw20_balance_as_native(&app, &token, &escrow);
+    assert_eq!(cw20.u128(), escrow_tokens);
+
+    // note: this fails as it uses query_all_balances, not the actual cw20 denom it got...
+    // https://github.com/CosmWasm/cosmwasm/blob/v1.2.6/contracts/hackatom/src/contract.rs#L92C36-L92C54
+    // we actually need a custom contract to properly test this part.
+
+    // // resolve the escrow, this should cause contract to transfer the cw20 tokens out to gov as "native"
+    // // note this include contract querying it's own balance as well as returning a BankMsg::Send
+    // let tx = TxBuilder::new()
+    //     .with_msg(WasmMsg::Execute {
+    //         sender: verifier.clone(),
+    //         contract_addr: escrow.clone(),
+    //         msg: to_json_binary(&super::hackatom::msgs::ExecuteMsg::Release {}).unwrap(),
+    //         funds: vec![],
+    //     })
+    //     .with_signer(&verify_key, 0);
+    // let res = app.block(&[tx]);
+    // assert_block_success(&res, 1);
+
+    // // verify balances - funds were transfered as part of the return message
+    // let cw20 = query_cw20_balance_as_native(&app, &token, &sender);
+    // assert_eq!(cw20.u128(), init_tokens - escrow_tokens);
+    // let cw20 = query_cw20_balance_as_native(&app, &token, &gov);
+    // assert_eq!(cw20.u128(), escrow_tokens); // moved to the recipient
+    // let cw20 = query_cw20_balance_as_native(&app, &token, &escrow);
+    // assert_eq!(cw20.u128(), 0); // nothing left in escrow
+}
+
+// This will instantiate a new hackatom instance from the given code and given initial balance,
+// and return the contract address. Will panic on error
+#[track_caller]
+fn init_hackatom(
+    app: &mut TestApp,
+    signer: &PrivateKey,
+    verifier: &AccountId,
+    beneficiary: &AccountId,
+    funds: u128,
+    denom: &str,
+) -> AccountId {
+    let sender = signer.account_id();
+    let sequence = app.sequence(&sender).unwrap();
+
+    let msg = WasmMsg::StoreCode {
+        sender: sender.clone(),
+        code: HACKATOM.into(),
+    };
+    let tx = TxBuilder::new()
+        .with_msg(msg)
+        .with_signer(&signer, sequence)
+        .with_fee(1_000_000, coin(50_000, DENOM));
+    let res = app.block(&[tx]);
+    assert_block_success(&res, 1);
+
+    // parse code_id from first message of first tx
+    let events = msg_events(&res[0], 0);
+    let code_id = event_value(events, "store_code", "code_id")
+        .unwrap()
+        .parse()
+        .unwrap();
+
+    // find the proper sequence
+    let sequence = app.sequence(&sender).unwrap();
+    let init_msg = super::hackatom::msgs::InstantiateMsg {
+        verifier: verifier.to_string(),
+        beneficiary: beneficiary.to_string(),
+    };
+    let msg = WasmMsg::Instantiate {
+        sender: sender.clone(),
+        admin: Some(sender),
+        code_id,
+        msg: to_json_binary(&init_msg).unwrap(),
+        funds: coins(funds, denom),
+        label: "Hackatom Contract".into(),
+    };
+    let tx = TxBuilder::new().with_msg(msg).with_signer(signer, sequence);
+    let mut res = app.block(&[tx]);
+    assert_block_success(&res, 1);
+
+    // parse address from first message of first tx
+    let events = msg_events(&res[0], 0);
+    let contract =
+        AccountId::parse_string(event_value(events, "instantiate", "_contract_address").unwrap())
+            .unwrap();
+
+    // ensure it matches the data field
+    assert_eq!(
+        res.remove(0).result.unwrap().data.remove(0),
+        MsgData::Wasm(WasmMsgData::Instantiate {
+            contract: contract.clone(),
+            data: b"".into()
+        })
+    );
+
+    contract
 }
