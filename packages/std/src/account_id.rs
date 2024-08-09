9247d7cf88976aa8481b369c9a576ccac3393d88
Ethan Frey <ethanfrey@noreply.github.com>
2024-08-09 13:25:08 +0200

    Start testing JSON-encoding of AccountId

--- packages/std/src/account_id.rs
+++ packages/std/src/account_id.rs
@@ -174,3 +174,56 @@ impl KeyDeserialize for &AccountId {
         Ok(AccountId(value))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use cosmwasm_std::{from_json, to_json_binary};
+
+    use super::*;
+
+    #[test]
+    fn test_creation() {
+        // properly parses and encoded proper size
+        let id = AccountId::new(&[42u8; 20]).unwrap();
+        assert!(id.to_string().starts_with("slay3r1"));
+        let reparse = AccountId::parse_string(&id.to_string()).unwrap();
+        assert_eq!(id, reparse);
+
+        // we can encode and decode valid addresses
+        let id = AccountId::new(&[69u8; 32]).unwrap();
+        assert!(id.to_string().starts_with("slay3r1"));
+        let reparse = AccountId::parse_string(&id.to_string()).unwrap();
+        assert_eq!(id, reparse);
+
+        // incorrect raw input fails
+        let _ = AccountId::new(&[69u8; 15]).unwrap_err();
+
+        // incorrect bedh32 input fails
+        let bad_addr = id.to_string().replace("q", "k");
+        let _ = AccountId::parse_string(&bad_addr).unwrap_err();
+    }
+
+    #[test]
+    fn test_json_encoding() {
+        let raw = [42u8; 20];
+        let id = AccountId::new(&raw).unwrap();
+
+        let as_string = to_json_binary(&id.to_string()).unwrap();
+        assert!(as_string.starts_with(br#""slay3r1"#));
+
+        let as_raw = to_json_binary(&raw).unwrap();
+        assert!(as_raw.starts_with(b"[42,42,"));
+
+        // ensure we can decode back to the same value from raw
+        let parsed: AccountId = from_json(&as_raw).unwrap();
+        assert_eq!(parsed, id);
+
+        // ensure we can decode back to the same value from string
+        let parsed: AccountId = from_json(&as_string).unwrap();
+        assert_eq!(parsed, id);
+
+        // ensure we encode as string
+        let encoded = to_json_binary(&id).unwrap();
+        assert_eq!(encoded, as_string);
+    }
+}
