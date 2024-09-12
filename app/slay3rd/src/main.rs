fdf57de801e6144c8192e051357c3f8aba25ea84
David Komer <6406986+dakom@users.noreply.github.com>
2024-09-12 13:39:32 +0300

    cargo fmt

--- app/slay3rd/src/main.rs
+++ app/slay3rd/src/main.rs
@@ -142,7 +142,9 @@ async fn run_server<T: PersistentStorage + 'static + Send + Sync>(
     let query = server.query_dispatcher();
 
     let grpc_reflection = tonic_reflection::server::Builder::configure()
-        .register_encoded_file_descriptor_set(include_bytes!("../../../packages/proto/src/protos/service_descriptor.bin"))
+        .register_encoded_file_descriptor_set(include_bytes!(
+            "../../../packages/proto/src/protos/service_descriptor.bin"
+        ))
         .build_v1()
         .unwrap();
 
