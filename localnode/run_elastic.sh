ba6cbb5981e7ba32deeae212364dd91b8f2d5f38
abefernan <44572727+abefernan@users.noreply.github.com>
2024-09-16 14:00:05 +0200

    Fix typo

--- localnode/run_elastic.sh
+++ localnode/run_elastic.sh
@@ -17,6 +17,6 @@ echo -n "Increasing tag size..."
 
 SPAN_INDEX=$(until curl http://localhost:9200/_cat/indices 2> /dev/null | grep span | cut -d " " -f3 | grep -m 1 "jaeger-span-"; do sleep 1 ; done)
 
-curl -sX PUT "localhost:9200/$SPAN_INDEX/_mapping?pretty" -H 'Content-Type: application/json' -d '{"dynamic_templates": [{"span_tags_map": {"path_match": "tag.*","mapping": {"ignore_above": 2052,"type": "keyword"}}}],"properties": {"tags": {"type": "nested","dynamic": "false","properties": {"key": {"type": "keyword","ignore_above": 2048},"tagType": {"type": "keyword","ignore_above": 2048},"value": {"type": "keyword","ignore_above": 2048}}}}}' > /dev/null
+curl -sX PUT "localhost:9200/$SPAN_INDEX/_mapping?pretty" -H 'Content-Type: application/json' -d '{"dynamic_templates": [{"span_tags_map": {"path_match": "tag.*","mapping": {"ignore_above": 2048,"type": "keyword"}}}],"properties": {"tags": {"type": "nested","dynamic": "false","properties": {"key": {"type": "keyword","ignore_above": 2048},"tagType": {"type": "keyword","ignore_above": 2048},"value": {"type": "keyword","ignore_above": 2048}}}}}' > /dev/null
 
 echo " done!"
