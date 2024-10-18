42637c26fd38153f84e961f0aab688058767da37
Hieu Vu <gundamaster5@gmail.com>
2024-10-18 16:44:40 +0700

    update local script node

--- local_node.sh
+++ local_node.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+mkdir -p ~/.slay3r
 cometbft init --home ~/.slay3r
-nano slay3r.toml
 
-nano ~/.slay3r/config/genesis.json
+cp ./genesis.json ~/.slay3r/config/.
 
-cometbft start --home ~/.slay3r
+cp ./slay3r.toml ~/.slay3r/config/.
+
+cp ./priv_validation.json ~/.slay3r/config/.
+
 slay3rd --log debug
+
+# run this at a different terminal
+# cometbft start --home ~/.slay3r
