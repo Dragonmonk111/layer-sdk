b6fd5ccbad94f42dfc369c5e9c6051c8a76c525b
Noah Saso <noahsaso@gmail.com>
2024-10-23 13:12:08 -0400

    setup ollama docker

--- localnode/stop.sh
+++ localnode/stop.sh
@@ -9,5 +9,5 @@ fi
 
 cd "$SCRIPT_DIR"
 # maybe overkill (if we just did run.sh) but it will ensure we stop everything
-$SUDO docker compose -f docker-compose.yml -f jaeger-elastic-compose.yml \
+$SUDO docker compose -f docker-compose.yml -f jaeger-elastic-compose.yml -f docker-compose.ollama.yml \
   --profile faucet --profile wasmatic down
